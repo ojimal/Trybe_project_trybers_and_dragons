7a4f585a13193d7ab88c1a459cded6687352f81c
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-05-07 19:34:22 +0200

    feat create extended Race classes

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Halfing extends Race {
+  private _maxLifePoints: number;
+  private static _createdInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfing._createdInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfing._createdInstances;
+  }
+}
