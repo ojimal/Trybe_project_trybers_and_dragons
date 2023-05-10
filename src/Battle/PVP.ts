ed753a716e5c344228c4502abe8e5fd287fc0dcd
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-05-10 18:56:41 +0200

    feat create extended class PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,28 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _hero: Fighter;
+  private _opponent: Fighter;
+
+
+  constructor(hero: Fighter, opponent: Fighter) {
+    super(hero);
+    this._hero = hero;
+    this._opponent = opponent;
+    super.fight();
+  }
+
+  fight(): number {
+    while (this._hero.lifePoints > 0 && this._opponent.lifePoints > 0) {
+      this._hero.attack(this._opponent);
+      this._opponent.attack(this._hero);
+    }
+  
+    if (this._hero.lifePoints <= 0) {
+      return -1; // Hero lost
+    } else {
+      return 1; // Hero won
+    }
+  }
+}
